78a32d78f3751c075f5c6ddd5312867de80d9d0f
cess <pmariate@up.edu.ph>
2024-05-13 16:05:05 +0800

    ui : finished organization sign up

--- lib/api/firebase_donors_api.dart
+++ lib/api/firebase_donors_api.dart
@@ -1,19 +1,19 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 
-class FirebaseTodoAPI {
-  static final FirebaseFirestore db = FirebaseFirestore.instance;
+// class FirebaseTodoAPI {
+//   static final FirebaseFirestore db = FirebaseFirestore.instance;
 
-// todo = user
-  Future<String> addTodo(Map<String, dynamic> user) async {
-    try {
-      await db.collection("users").add(user);
+// // todo = user
+//   Future<String> addTodo(Map<String, dynamic> user) async {
+//     try {
+//       await db.collection("users").add(user);
 
-      return "Successfully added!";
-    } on FirebaseException catch (e) {
-      return "Error in ${e.code}: ${e.message}";
-    }
-  }
+//       return "Successfully added!";
+//     } on FirebaseException catch (e) {
+//       return "Error in ${e.code}: ${e.message}";
+//     }
+//   }
 
 
 
-}
+// }
