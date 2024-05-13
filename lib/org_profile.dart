3c320d499d3fd195ac52365e2e731b86ac331de1
Jajabeenka <jacarranza@up.edu.ph>
2024-05-13 15:45:00 +0800

    added first iteration of org profile, used listview for list of charities then singlechildscrollview for the about part to consider long descriptions

--- lib/org_profile.dart
+++ lib/org_profile.dart
@@ -10,46 +10,317 @@ class OrgProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Organization Profile',
-              style: TextStyle(color: Color(0xFFFFC107)),
+        title: const Text('Organization Profile', 
+              style: TextStyle(color: Colors.white),
               ),
         backgroundColor: const Color.fromARGB(255, 8, 64, 60),
       ),
-      body: Column(
-        children: [ 
-          SingleChildScrollView(
-            child: Container(
-              color: const Color(0xFF9F1010),
-              padding: const EdgeInsets.all(10.0),
-              width: MediaQuery.of(context).size.width,
-              height: 175.0,
-              child: const Column(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color(0xFF9F1010),
+        ),
+        child: Column(
+          children: [ 
+            SingleChildScrollView(
+              child: Container(
+                margin: const EdgeInsets.all(6.0),
+                padding: const EdgeInsets.all(7.0),
+                width: MediaQuery.of(context).size.width,
+                height: 175.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  border: Border.all(
+                    width: 5,
+                    color: const Color(0xFFFFC107),
+                  )
+                ),
+                child: const Column(
                 children: [
-                  //edit button
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: SizedBox(
-                      width: 30.0,
-                      height: 30.0,
-                      child: FittedBox(
-                        child: FloatingActionButton(
-                          onPressed: null,
-                          child: Icon(CupertinoIcons.pencil)
-                        ),
-                      ),
-                    )
-                  ),
+                  //org logo and name
                   Row(
                     children: [
-                      Text('<logo>', style: TextStyle(color: Color(0xFFFFC107))),
+                      Text('<logo>', style: TextStyle(color: Color(0xFF9F1010))),
                       SizedBox(width: 20.0),
-                      Text('<Charity Name>', style: TextStyle(color: Color(0xFFFFC107))),
-                    ],)
+                      Text('Elgives Charity', style: TextStyle(color: Color(0xFF9F1010), fontWeight: FontWeight.bold)),
+                      Spacer(),
+                        //edit button
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: SizedBox(
+                          width: 30.0,
+                          height: 30.0,
+                          child: FittedBox(
+                            child: FloatingActionButton(
+                              onPressed: null,
+                              child: Icon(Icons.edit),
+                            ),
+                          ),
+                        )
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 15.0),
+                  Row(
+                    children: [
+                      Text('About', style: TextStyle(color: Color(0xFF9F1010), fontSize: 25, fontWeight: FontWeight.bold)),
+                    ],
+                  ),                  
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', style: TextStyle(color: Color.fromARGB(255, 0, 0, 0))),
+                    ),
+                  ),
                 ],
               ),
+              )
+            ),
+            //list of charities
+            Expanded (
+              child: ListView(
+                children: [
+                  Container (
+                    margin: const EdgeInsets.all(6.0),
+                    padding: const EdgeInsets.all(7.0),
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.black,
+                      )
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Charity Name", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text("OPEN", style: TextStyle(color: Color.fromARGB(255, 8, 64, 60), fontSize: 20, fontWeight: FontWeight.bold)),
+                      ],),
+                  ),
+                  Container (
+                    margin: const EdgeInsets.all(6.0),
+                    padding: const EdgeInsets.all(7.0),
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.black,
+                      )
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Charity Name", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text("OPEN", style: TextStyle(color: Color.fromARGB(255, 8, 64, 60), fontSize: 20, fontWeight: FontWeight.bold)),
+                      ],),
+                  ),
+                  Container (
+                    margin: const EdgeInsets.all(6.0),
+                    padding: const EdgeInsets.all(7.0),
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.black,
+                      )
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Charity Name", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text("CLOSED", style: TextStyle(color: Color(0xFF9F1010), fontSize: 20, fontWeight: FontWeight.bold)),
+                      ],),
+                  ),
+                  Container (
+                    margin: const EdgeInsets.all(6.0),
+                    padding: const EdgeInsets.all(7.0),
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.black,
+                      )
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Charity Name", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text("CLOSED", style: TextStyle(color: Color(0xFF9F1010), fontSize: 20, fontWeight: FontWeight.bold)),
+                      ],),
+                  ),
+                  Container (
+                    margin: const EdgeInsets.all(6.0),
+                    padding: const EdgeInsets.all(7.0),
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.black,
+                      )
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Charity Name", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text("OPEN", style: TextStyle(color: Color.fromARGB(255, 8, 64, 60), fontSize: 20, fontWeight: FontWeight.bold)),
+                      ],),
+                  ),
+                  Container (
+                    margin: const EdgeInsets.all(6.0),
+                    padding: const EdgeInsets.all(7.0),
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.black,
+                      )
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Charity Name", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text("CLOSED", style: TextStyle(color: Color(0xFF9F1010), fontSize: 20, fontWeight: FontWeight.bold)),
+                      ],),
+                  ),
+                  Container (
+                    margin: const EdgeInsets.all(6.0),
+                    padding: const EdgeInsets.all(7.0),
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.black,
+                      )
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Charity Name", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text("CLOSED", style: TextStyle(color: Color(0xFF9F1010), fontSize: 20, fontWeight: FontWeight.bold)),
+                      ],),
+                  ),
+                  Container (
+                    margin: const EdgeInsets.all(6.0),
+                    padding: const EdgeInsets.all(7.0),
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.black,
+                      )
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Charity Name", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text("CLOSED", style: TextStyle(color: Color(0xFF9F1010), fontSize: 20, fontWeight: FontWeight.bold)),
+                      ],),
+                  ),
+                  Container (
+                    margin: const EdgeInsets.all(6.0),
+                    padding: const EdgeInsets.all(7.0),
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.black,
+                      )
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Charity Name", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text("OPEN", style: TextStyle(color: Color.fromARGB(255, 8, 64, 60), fontSize: 20, fontWeight: FontWeight.bold)),
+                      ],),
+                  ),
+                  Container (
+                    margin: const EdgeInsets.all(6.0),
+                    padding: const EdgeInsets.all(7.0),
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.black,
+                      )
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Charity Name", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text("OPEN", style: TextStyle(color: Color.fromARGB(255, 8, 64, 60), fontSize: 20, fontWeight: FontWeight.bold)),
+                      ],),
+                  ),
+                  Container (
+                    margin: const EdgeInsets.all(6.0),
+                    padding: const EdgeInsets.all(7.0),
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.black,
+                      )
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Charity Name", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text("OPEN", style: TextStyle(color: Color.fromARGB(255, 8, 64, 60), fontSize: 20, fontWeight: FontWeight.bold)),
+                      ],),
+                  ),
+                ],
+              )
+            ),
+            Container(
+              margin: const EdgeInsets.all(6.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: 30.0,
+                    height: 30.0,
+                    child: FittedBox(
+                      child: FloatingActionButton(
+                        onPressed: null,
+                        child: Icon(Icons.arrow_back),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 30.0,
+                    height: 30.0,
+                    child: FittedBox(
+                      child: FloatingActionButton(
+                        onPressed: null,
+                        child: Icon(Icons.arrow_forward),
+                      ),
+                    ),
+                  ),
+                ]
+              ),
             )
-          )
-        ],
+          ],
+        ),
       ),
 
     );
@@ -63,3 +334,5 @@ class OrgProfile extends StatelessWidget {
         //     // Navigate to second route
         //     },
         //   ),
+
+        
