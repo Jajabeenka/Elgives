fb9a72246decf4d1cbacd074d120fefd634c7f1c
Jajabeenka <jacarranza@up.edu.ph>
2024-05-15 17:20:52 +0800

    commit

--- lib/api/firebase_donor_api.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class FirebaseTodoAPI {
-  static final FirebaseFirestore db = FirebaseFirestore.instance;
-
-  Stream<QuerySnapshot> getAllDonors() {
-    return db.collection("donors").snapshots();
-  }
-}
